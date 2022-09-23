89e13dad3d5cc5ace30e706205399ab2a8c76cb6
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-09-23 16:14:22 -0400

    base setup

---
+++ server/README.md
@@ -0,0 +1,5 @@
+## Creating a Server with Node.js
+
+### Importing modules 
+Give the item a variable name, use require('module_name_here');
+If you are going to be importing on of your own files as a module, make sure you have a relative path to that file. (./)
---
+++ server/app.js
@@ -0,0 +1,12 @@
+// import modules 
+const http = require('http'); // built in module
+
+// function reqListener(req, res) {
+// }; // this executes on any incoming requests as a callback. Hoover over the createServer for details
+// http.createServer(reqListener); // refactor 
+
+const server = http.createServer((req, res) => {
+  console.log(req); 
+});
+
+server.listen(3000) // start a process 

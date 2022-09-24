15ba0de4c589a2d3f12787d7e6646a338be739d5
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-09-24 16:36:06 -0400

    create frame work for working with POST

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 // import modules 
 const http = require('http'); // built in module
+const fs = require('fs'); //file sync
 
 // function reqListener(req, res) {
 // }; // this executes on any incoming requests as a callback. Hoover over the createServer for details
@@ -7,23 +8,35 @@ const http = require('http'); // built in module
 
 const server = http.createServer((req, res) => {
   // console.log(req); 
-  const url = req.url; 
+
   console.log(req.url, req.method, req.headers)
+
+  const url = req.url; 
+  const method = req.method;
+
   //process.exit(); // when you go to localhost:3000 after running it will now exit.
   if (url === '/') {
     res.setHeader('Content-Type', 'text/html'); //attach header with meta information. <formatting the write command
     res.write('<html>');//write some data to the response 
     res.write('<head><title>Enter Message</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" placeholder="enter your message here!"/><button type="submit">Send</button></form></body>');
+    res.write('<body><form action="/message" method="POST"><input id="message" type="text" placeholder="enter your message here!"/><button type="submit">Send</button></form></body>');
     res.write('</html>');
     return res.end(); 
-  }
-    // res.setHeader('Content-Type', 'text/html'); //attach header with meta information. <formatting the write command
-    // res.write('<html>');//write some data to the response 
-    // res.write('<head><title>My First Page</title></head>');
-    // res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    // res.write('</html>');
-    // res.end(); 
+  };
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync('message.txt', 'PLACEHOLDER FOR NOW'); 
+    res.statusCode = 302; 
+    res.setHeader('Location', '/'); 
+    return res.end(); 
+    // res.writeHead(302, {});//write meta information, 302 - redirect or use the above 
+  };
+    res.setHeader('Content-Type', 'text/html'); //attach header with meta information. <formatting the write command
+    res.write('<html>');//write some data to the response 
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end(); 
   //shouldn't write anything after it is end. This is how it will be sent back to the client. 
 
 });

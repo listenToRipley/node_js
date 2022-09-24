d30fcdf8e76136a6e829a40f113b3ca2df331d65
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-09-24 17:12:42 -0400

    create and redirect

--- server/app.js
+++ server/app.js
@@ -8,8 +8,7 @@ const fs = require('fs'); //file sync
 
 const server = http.createServer((req, res) => {
   // console.log(req); 
-
-  console.log(req.url, req.method, req.headers)
+  //console.log(req.url, req.method, req.headers)
 
   const url = req.url; 
   const method = req.method;
@@ -17,36 +16,36 @@ const server = http.createServer((req, res) => {
   //process.exit(); // when you go to localhost:3000 after running it will now exit.
   if (url === '/') {
     res.setHeader('Content-Type', 'text/html'); //attach header with meta information. <formatting the write command
-    res.write('<html>');//write some data to the response 
-    res.write('<head><title>Enter Message</title></head>');
-    res.write('<body><form action="/message" method="POST"><input id="message" type="text" placeholder="enter your message here!"/><button type="submit">Send</button></form></body>');
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title><head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
     res.write('</html>');
-    return res.end(); 
+    return res.end();
   };
 
-  if (url === "/message" && method === "POST") {
-
+  if (url === '/message' && method === 'POST') {
     const body = []; //ending the value within, but not the variable itself
 
-    res.on('data', (chunk) => {
+    req.on('data', (chunk) => {
       console.log('CHUNK >>\n',chunk); 
-      body.push(chunk);
-    }); //how use to listen to events
+      body.push(chunk); //how use to listen to events
+      
+    });
 
-    res.on('end', () => {
+    res.on('end', () => { //"stop" point
       //creating a buffer and interact with the data
       const parsedBody = Buffer.concat(body).toString();
       console.log('PARSE BODY\n', parsedBody); 
 
-      const message = parsedBody.split('=')[1]; 
-      fs.writeFileSync('message.txt', message); 
-
-    }); // after this data is done
-    res.statusCode = 302; 
-    res.setHeader('Location', '/'); 
-    return res.end(); 
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });// after this data is done
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
     // res.writeHead(302, {});//write meta information, 302 - redirect or use the above 
   };
+
     res.setHeader('Content-Type', 'text/html'); //attach header with meta information. <formatting the write command
     res.write('<html>');//write some data to the response 
     res.write('<head><title>My First Page</title></head>');

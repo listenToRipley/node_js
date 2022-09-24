d43d0033d0d1faffca68492f97c610d2f34fc469
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-09-24 16:04:59 -0400

    examples of res

--- server/app.js
+++ server/app.js
@@ -6,8 +6,10 @@ const http = require('http'); // built in module
 // http.createServer(reqListener); // refactor 
 
 const server = http.createServer((req, res) => {
-  console.log(req); 
-  process.exit();
+  // console.log(req); 
+  console.log(req.url, req.method, req.headers)
+  //process.exit(); // when you go to localhost:3000 after running it will now exit.
+  res.setHeader('Content-Type', 'text/html'); //attach header with meta information.
 });
 
 server.listen(3000); // start a process 

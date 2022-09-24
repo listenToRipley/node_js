deeebff204cd57db3e100f72918d1686a9d0a27a
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-09-24 16:24:34 -0400

    post message

--- server/app.js
+++ server/app.js
@@ -9,12 +9,20 @@ const server = http.createServer((req, res) => {
   // console.log(req); 
   console.log(req.url, req.method, req.headers)
   //process.exit(); // when you go to localhost:3000 after running it will now exit.
-  res.setHeader('Content-Type', 'text/html'); //attach header with meta information. <formatting the write command
-  res.write('<html>');//write some data to the response 
-  res.write('<head><title>My First Page</title></head>');
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  res.write('</html>');
-  res.end(); 
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html'); //attach header with meta information. <formatting the write command
+    res.write('<html>');//write some data to the response 
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" placeholder="enter your message here!"/><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    res.end(); 
+  }
+    res.setHeader('Content-Type', 'text/html'); //attach header with meta information. <formatting the write command
+    res.write('<html>');//write some data to the response 
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end(); 
   //shouldn't write anything after it is end. This is how it will be sent back to the client. 
 
 });

e497b8e05a073356fc4c012580551427f53ad1e2
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-09-26 09:39:00 -0400

    create new server with routes

--- server/app.js
+++ server/app.js
@@ -1,13 +1,16 @@
 // import modules 
 const http = require('http'); // built in module
+const routes = require('./routes'); //this is the local path to the new requirement
 
 // function reqListener(req, res) {
 // }; // this executes on any incoming requests as a callback. Hoover over the createServer for details
 // http.createServer(reqListener); // refactor 
 
-const server = http.createServer((req, res) => {
-  // console.log(req); 
-  //console.log(req.url, req.method, req.headers)
-});
+// const server = http.createServer((req, res) => {
+//   // console.log(req); 
+//   //console.log(req.url, req.method, req.headers)
+//   // information from router was previously housed in this body, but for clarify of purpose, it was moved to routes.js. 
+// });
+const server = http.createServer(routes); //tells node to execute the function stored in routes. 
 
 server.listen(3000); // start a process 

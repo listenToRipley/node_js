360918f3612ee1b63fc7b362a8af862f233a820a
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2022-09-26 14:03:24 -0400

    assignment 2 complete

--- assign2/app.js
+++ assign2/app.js
@@ -1,2 +1,26 @@
 const http = require('http'); 
 const express = require('express'); 
+const app = express(); 
+
+// app.use((req, res, next) => {
+//   console.log('You found the middleware!')
+//   next(); 
+// });
+
+// app.use((req, res, next) => {
+//   console.log('You found the second middleware')
+// });
+
+app.use('/users',(req, res, next) => {
+  console.log('You found the user middleware!')
+  res.send('<h1>You found the user page!</h1>');
+});
+
+app.use('/',(req, res, next) => {
+  console.log('You found the main middleware. This is the end of the line')
+  res.send('<h1>You found the main page!</h1>'); 
+});
+
+const server = http.createServer(app); 
+
+server.listen(3000); 

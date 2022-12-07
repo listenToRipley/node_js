8551dcd9cabadd209ea5d20be76ab48d4644300f
nken <nken@lle.rochester.edu>
2022-12-06 20:26:04 -0500

    added controller for errors

--- mvc/app.js
+++ mvc/app.js
@@ -10,6 +10,7 @@ app.set('views', 'views'); // how to see the location of the html and where the 
 
 const adminRoutes = require('./routes/admin'); // just the routes now. 
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error')
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,9 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes); // allows for ALL routes from adminData to be accessible.
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404', {pageTitle:'Page Not Found'});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
